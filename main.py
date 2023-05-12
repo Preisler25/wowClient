214d448cb4b55f1a2cb1f45fac652eb4d82d9839
Preisler <preisler.andras@gmail.com>
2023-05-12 22:21:11 +0200

    main

--- main.py
+++ main.py
@@ -10,6 +10,36 @@ def genRandomPos(s_w, s_h):
     return p_x, p_y
 
 
+def gameLoop():
+    pos_x, pos_y = genRandomPos(w, h)
+    # Game loop
+    game_exit = False
+    start_time = time.time()
+    counter = 0
+    while counter < 10 and not game_exit:
+
+        game_display.fill(black)
+        b = game_display.blit(button, (pos_x, pos_y))
+        # Displaying time and points
+        debug(f"Time: {time.time()-start_time}", display_width-170, 10)
+        debug(f"Points: {counter}", display_width/2-75, 10)
+
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                game_exit = True
+            elif event.type == pygame.MOUSEBUTTONDOWN and event.button == 1:
+                pos = pygame.mouse.get_pos()
+                if b.collidepoint(pos):
+                    pos_x, pos_y = genRandomPos(w, h)
+                    counter += 1
+            elif event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_UP:
+                    debug("alma")
+        pygame.display.update()
+
+    print("Game over!")
+
+
 pygame.init()
 
 # Display dimensions
@@ -27,42 +57,9 @@ pygame.display.set_caption('Game')
 button = pygame.image.load('graphics/Player/player_stand.png').convert_alpha()
 w = display_width-button.get_width()
 h = display_height-button.get_height()
-pos_x, pos_y = genRandomPos(w, h)
 
-# Game loop
-game_exit = False
-start_time = time.time()
-counter = 0
-while counter < 10 and not game_exit:
-
-    game_display.fill(black)
-    b = game_display.blit(button, (pos_x, pos_y))
-    # Displaying time
-    debug(f"Time: {time.time()-start_time}")
-    debug(f"Points: {counter}", 10, 30)
-
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            game_exit = True
-        elif event.type == pygame.MOUSEBUTTONDOWN and event.button == 1:
-            pos = pygame.mouse.get_pos()
-            if b.collidepoint(pos):
-                pos_x, pos_y = genRandomPos(w, h)
-                counter += 1
-        elif event.type == pygame.KEYDOWN:
-            if event.key == pygame.K_UP:
-                debug("alma")
-
-    # Draw game objects
-    # ...
-
-    # Update display
-    pygame.display.update()
-
-# do something
-end_time = time.time()-start_time
-debug(f"Time: {end_time}")
-print(f"Time: {end_time}")
+gameLoop()
+gameLoop()
 
 # Quit Pygame
 pygame.quit()
