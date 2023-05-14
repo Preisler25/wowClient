10777e51ca9d846e021815af4049425349e5e711
Preisler <preisler.andras@gmail.com>
2023-05-14 08:40:57 +0200

    main and game loop doc

--- main.py
+++ main.py
@@ -178,46 +178,71 @@ def settings_page():
 
 
 def gameLoop(best_time):
-    # Load enemy
-    enemy = Char('graphics/Player/player_stand.png')
-    enemy.setRandomPos(display_width, display_height)
+    game_exit = False
     pygame.display.set_caption('Game')
 
-    # Game loop
-    game_exit = False
+    # Load enemy
+    enemy = Char('graphics/Player/player_stand.png')
+    # Setting enemy position randomly but not out of the screen
+    enemy.setRandomPos(display_width, display_height)
+
+    # getting start time
     start_time = time.time()
+
+    # setting counter
     counter = 0
+
+    # loading background
     bg = pygame.image.load("graphics/menu_bg.jpeg")
+
+    # Game loop
     while counter < 10 and not game_exit:
 
+        # drawing game
+        # drawing background
         game_display.blit(bg, (0, 0))
+        # drawing enemy
         enemy.draw()
+        # drawing time
         showText(f"Time: {time.time()-start_time}", display_width/2-200, 40)
+        # drawing counter
         showText(f"Remaining: {10-counter}", display_width/2-75, 10)
 
+        # Event handling
         for event in pygame.event.get():
+            # quits
             if event.type == pygame.QUIT:
                 game_exit = True
+            # mouse click
             elif event.type == pygame.MOUSEBUTTONDOWN and event.button == 1:
+                # getting mouse position
                 pos = pygame.mouse.get_pos()
+                # if enemy pressed
                 if enemy.rect.collidepoint(pos):
+                    # setting new enemy position
                     enemy.setRandomPos(display_width, display_height)
                     print(enemy)
+                    # increasing counter
                     counter += 1
+            # keydown
             elif event.type == pygame.KEYDOWN:
                 if event.key == pygame.K_UP:
                     debug("alma")
+
+        # Update display
         pygame.display.update()
 
+    # getting end time
     endTime = time.time()-start_time
+    # testing for new best time
     if endTime < best_time:
         best_time = endTime
     return best_time, endTime
 
 
+# ------------------Main------------------
+ # Initialize Pygame
 pygame.init()
-
-clock = pygame.time.Clock()
 
 # Display dimensions
 display_width = 800
@@ -229,7 +254,6 @@ white = (255, 255, 255)
 
 # Create game display
 game_display = pygame.display.set_mode((display_width, display_height))
-
 
 # Lobby loop
 lobby()
