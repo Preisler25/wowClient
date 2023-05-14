f84c3998e50cd4d0d9aa9201456050c75ebedd25
Preisler <preisler.andras@gmail.com>
2023-05-14 08:16:16 +0200

    settings doc

--- main.py
+++ main.py
@@ -53,6 +53,8 @@ def dotCh(text, count, dt):
         count += 1*dt
     return text, count
 
+# ------------------Lobby------------------
+
 
 def lobby(best_time=100, end_time=100):
     game_exit = False
@@ -66,12 +68,10 @@ def lobby(best_time=100, end_time=100):
     amplitude = 12
     frequency = 0.2
 
-    # settings button size
-    settings_btn_size = 70
-
     # creating settings button
-    settings_btn = PressBtn(display_width-settings_btn_size, 0,
-                            settings_btn_size, settings_btn_size, "graphics/settings.png")
+    settings_btn_icon_size = 70
+    settings_btn = PressBtn(display_width-settings_btn_icon_size, 0,
+                            settings_btn_icon_size, settings_btn_icon_size, "graphics/settings.png")
 
     # loading background
     bg = pygame.image.load("graphics/menu_bg.jpeg")
@@ -127,6 +127,7 @@ def lobby(best_time=100, end_time=100):
                     pygame.display.set_caption('Lobby')
             # mouse click
             elif event.type == pygame.MOUSEBUTTONDOWN and event.button == 1:
+                # getting mouse position
                 pos = pygame.mouse.get_pos()
                 # settings
                 if settings_btn.rect.collidepoint(pos):
@@ -134,30 +135,46 @@ def lobby(best_time=100, end_time=100):
         # Update display
         pygame.display.update()
 
+# ------------------Settings------------------
+
 
 def settings_page():
     game_exit = False
     pygame.display.set_caption('Settings')
 
+    # loading background
     bg = pygame.image.load("graphics/menu_bg.jpeg")
 
+    # creating back button
     back_icon_size = 50
     back_btn = PressBtn(0, 0, back_icon_size,
                         back_icon_size, "graphics/back.jpg")
 
+    # display loop
     while not game_exit:
+
+        # drawing settings
+        # drawing background
         game_display.blit(bg, (0, 0))
+        # back button
         back_btn.darw()
 
+        # Event handling
         for event in pygame.event.get():
+            # quit
             if event.type == pygame.QUIT:
                 game_exit = True
+            # mouse click
             if event.type == pygame.MOUSEBUTTONDOWN and event.button == 1:
+                # getting mouse position
                 pos = pygame.mouse.get_pos()
+                # back
                 if back_btn.rect.collidepoint(pos):
                     game_exit = True
 
         pygame.display.update()
+
+# ------------------Game------------------
 
 
 def gameLoop(best_time):
